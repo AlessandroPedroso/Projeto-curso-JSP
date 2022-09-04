f67217694dc2315698fb4dc709621e95a30a6996
Alessandro Pedroso <alessandropedrosoti@gmail.com>
2022-09-04 20:30:32 -0300

    Criando o SQL que atualiza no banco de dados

--- curso-jsp/src/main/java/dao/DAOUsuarioRepository.java
+++ curso-jsp/src/main/java/dao/DAOUsuarioRepository.java
@@ -19,7 +19,10 @@ public class DAOUsuarioRepository {
 	/*faz o inser no banco da página de cadastro usuario.jsp*/
 	public ModelLogin gravarUser(ModelLogin objeto) throws Exception {
 		
-		String sql = "INSERT INTO model_login(login, senha, nome, email) VALUES (?, ?, ?, ?)";
+		if(objeto.isNovo()) {/*Grava um novo*/
+			
+		
+			String sql = "INSERT INTO model_login(login, senha, nome, email) VALUES (?, ?, ?, ?)";
 	
 			PreparedStatement insert = connection.prepareStatement(sql);
 			insert.setString(1, objeto.getLogin());
@@ -28,7 +31,18 @@ public class DAOUsuarioRepository {
 			insert.setString(4, objeto.getEmail());
 			insert.execute();
 			connection.commit();
+		}else {
 			
+			String sql = "UPDATE model_login SET login=?, senha=?, nome=?, email=? WHERE id = " + objeto.getId() +"";
+			PreparedStatement preparaSql = connection.prepareStatement(sql);
+			preparaSql.setString(1, objeto.getLogin());
+			preparaSql.setString(2, objeto.getSenha());
+			preparaSql.setString(3, objeto.getNome());
+			preparaSql.setString(4, objeto.getEmail());
+			preparaSql.executeUpdate();
+			connection.commit();
+			
+		}
 			return this.consultaUsuario(objeto.getLogin());
 		
 	}
@@ -52,6 +66,25 @@ public class DAOUsuarioRepository {
 		}
 		
 		return modelLogin;
+		
+	}
+	
+	public boolean validaLogin(String login) throws Exception {
+		
+		String sql = "select count(1) > 0 as existe from model_login where upper(login) = upper('"+login+"')";
+		PreparedStatement statement = connection.prepareStatement(sql);
+		
+		ResultSet resultSet = statement.executeQuery();
+		
+		/*if(resultSet.next()) {
+			
+			return resultSet.getBoolean("existe");
+		}
+		*/
+		
+		resultSet.next()/*Pra ele entrar nos resultados do sql e virificar*/;
+		return resultSet.getBoolean("existe");
+		
 		
 	}
 
--- curso-jsp/src/main/java/servlets/ServletUsuarioController.java
+++ curso-jsp/src/main/java/servlets/ServletUsuarioController.java
@@ -33,26 +33,36 @@ public class ServletUsuarioController extends HttpServlet {
 		
 		try {
 		
-		String id = request.getParameter("id");
-		String nome = request.getParameter("nome");
-		String email = request.getParameter("email");
-		String login = request.getParameter("login");
-		String senha = request.getParameter("senha");
-		
-		ModelLogin modelLogin = new ModelLogin();
-		modelLogin.setId(id !=null && !id.isEmpty() ? Long.parseLong(id) : null);
-		modelLogin.setNome(nome);
-		modelLogin.setEmail(email);
-		modelLogin.setLogin(login);
-		modelLogin.setSenha(senha);
-		
-		modelLogin = daoUsuarioRepository.gravarUser(modelLogin);
-		
-		request.setAttribute("msg", "Operação realizada com sucesso!");
-		request.setAttribute("modelLogin", modelLogin);
-		
-	    RequestDispatcher redireciona = request.getRequestDispatcher("principal/usuario.jsp");
-	    redireciona.forward(request, response);
+					String msg = "Operação realizada com sucesso!";
+					
+					String id = request.getParameter("id");
+					String nome = request.getParameter("nome");
+					String email = request.getParameter("email");
+					String login = request.getParameter("login");
+					String senha = request.getParameter("senha");
+					
+					ModelLogin modelLogin = new ModelLogin();
+					modelLogin.setId(id !=null && !id.isEmpty() ? Long.parseLong(id) : null);
+					modelLogin.setNome(nome);
+					modelLogin.setEmail(email);
+					modelLogin.setLogin(login);
+					modelLogin.setSenha(senha);
+					
+					if(daoUsuarioRepository.validaLogin(modelLogin.getLogin()) && modelLogin.getId() == null){
+						
+						msg = "Já existe usuário com o mesmo login, informe outro login!";
+						
+					}else {
+						
+						modelLogin = daoUsuarioRepository.gravarUser(modelLogin);
+						
+					}
+					
+					request.setAttribute("msg", msg);
+					request.setAttribute("modelLogin", modelLogin);
+					
+				    RequestDispatcher redireciona = request.getRequestDispatcher("principal/usuario.jsp");
+				    redireciona.forward(request, response);
 	    
 		}catch (Exception e) {
 			
