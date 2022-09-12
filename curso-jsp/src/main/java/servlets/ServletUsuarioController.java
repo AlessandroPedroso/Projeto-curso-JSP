554ac171ad64b9951a76441543b16ecad6aefa32
Alessandro Pedroso <alessandropedrosoti@gmail.com>
2022-09-12 16:56:40 -0300

    Upload imagem usuário - Parte 2

--- curso-jsp/src/main/java/servlets/ServletUsuarioController.java
+++ curso-jsp/src/main/java/servlets/ServletUsuarioController.java
@@ -6,10 +6,15 @@ import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
 import model.ModelLogin;
 
 import java.io.IOException;
 import java.util.List;
+
+import org.apache.tomcat.jakartaee.commons.compress.utils.IOUtils;
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -135,6 +140,14 @@ public class ServletUsuarioController extends ServletGenericUtil {
 					modelLogin.setSenha(senha);
 					modelLogin.setPerfil(perfil);
 					modelLogin.setSexo(sexo);
+					
+					if (ServletFileUpload.isMultipartContent(request)) {
+						
+						Part part = request.getPart("filefoto"); /*pega foto da tela*/
+						byte[] foto = IOUtils.toByteArray(part.getInputStream()); /*converte imagem para byte*/
+						String imagemBase64 = new Base64().encodeBase64String(foto);
+						
+					}
 					
 					if(daoUsuarioRepository.validaLogin(modelLogin.getLogin()) && modelLogin.getId() == null){
 						
