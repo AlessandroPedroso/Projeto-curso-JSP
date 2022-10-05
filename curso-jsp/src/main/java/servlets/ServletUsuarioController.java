457e70f2751b666f4b854bd5fe7cace5635681f3
Alessandro Pedroso <alessandropedrosoti@gmail.com>
2022-10-04 23:51:16 -0300

    Formatando Data e Money e correção de BUG

--- curso-jsp/src/main/java/servlets/ServletUsuarioController.java
+++ curso-jsp/src/main/java/servlets/ServletUsuarioController.java
@@ -200,7 +200,10 @@ public class ServletUsuarioController extends ServletGenericUtil {
 					modelLogin.setLocalidade(localidade);
 					modelLogin.setUf(uf);
 					modelLogin.setNumero(numero);
-					modelLogin.setDataNascimento(new Date(new SimpleDateFormat("dd/mm/yyyy").parse(dataNascimento).getTime()));
+					
+					//Date.valueOf(new SimpleDateFormat("yyyy-mm-dd").format(new SimpleDateFormat("dd/mm/yyyy").parse(dataNascimento)));
+					
+					modelLogin.setDataNascimento(Date.valueOf(new SimpleDateFormat("yyyy-mm-dd").format(new SimpleDateFormat("dd/mm/yyyy").parse(dataNascimento))));
 					modelLogin.setRendamensal(Double.parseDouble(rendaMensal));
 					
 					if (ServletFileUpload.isMultipartContent(request)) {
